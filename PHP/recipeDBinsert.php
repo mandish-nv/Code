076b1b6565d4d8e7816a29b31bc25535326e6c7e
mandish-nv <mandishvaidya12345@gmail.com>
2024-11-24 10:45:18 +0545

    Redirect to main page after form submission.

--- PHP/recipeDBinsert.php
+++ PHP/recipeDBinsert.php
@@ -13,35 +13,35 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   $tempname = $_FILES['recipeImage']['tmp_name'];
   $folder = '../uploadedImages/' . $file_name;
   move_uploaded_file($tempname, $folder);
-  echo "File uploaded successfully.";
+  //echo "File uploaded successfully.";
 
   //ingredientsRequired
   $ingredientsRequired = $_POST['ingredientsRequired'];
   $file_name_ir = "ingredientsRequired-".$recipeName."-".$chefName.".txt";
   $file_path = "../uploadedTexts/uploadedIngredientsRequired/".$file_name_ir;
   file_put_contents($file_path, $ingredientsRequired);
-  echo "Text has been saved to " . $file_path;
+  //echo "Text has been saved to " . $file_path;
 
   //procedure
   $procedureSteps = $_POST['procedure'];
   $file_name_ps = "procedureSteps-".$recipeName."-".$chefName.".txt";
   $file_path = "../uploadedTexts/uploadedProcedureSteps/".$file_name_ps;
   file_put_contents($file_path, $procedureSteps);
-  echo "Text has been saved to " . $file_path;
+  //echo "Text has been saved to " . $file_path;
 
   //nutritionalFacts
   $nutritionalFacts = $_POST['nutritionalFacts'];
   $file_name_nf = "nutritionalFacts-".$recipeName."-".$chefName.".txt";
   $file_path = "../uploadedTexts/uploadedNutritionalFacts/".$file_name_nf;
   file_put_contents($file_path, $nutritionalFacts);
-  echo "Text has been saved to " . $file_path;
+  //echo "Text has been saved to " . $file_path;
 
   //tipsTricks
   $tipsTricks = $_POST['tipsTricks'];
   $file_name_tt = "tipsTricks-".$recipeName."-".$chefName.".txt";
   $file_path = "../uploadedTexts/uploadedTipsTricks/".$file_name_tt;
   file_put_contents($file_path, $tipsTricks);
-  echo "Text has been saved to " . $file_path;
+  //echo "Text has been saved to " . $file_path;
 
   $servername = "localhost";
   $username = "root";
@@ -51,10 +51,15 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   $conn = mysqli_connect($servername, $username, $password, $database);
 
   $sql = "insert into recipe values ('" . $recipeName . "','" . $chefName . "','" . $category . "','" . $difficultyLevel . "'," . $servingPortion . "," . $timeRequiredHour . "," . $timeRequiredMinute . ",'" . $file_name_ir . "','" . $file_name_ps . "','" . $file_name_nf . "','" . $file_name_tt . "','" . $file_name . "',0);";
-  echo $sql;
+  //echo $sql;
   mysqli_query($conn, $sql);
 
   mysqli_close($conn);
+
+   // Redirect to an HTML file
+   //no HTML or echo statements before header()
+   header("Location: ../HTML/index.html");
+   exit();
 } else {
   echo "Invalid request method.";
 }
